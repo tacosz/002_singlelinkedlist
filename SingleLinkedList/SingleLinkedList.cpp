91359c9b9951bbdc8807016c011058b70484264a
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-25 19:34:31 +0700

    penambahan bool

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -82,6 +82,12 @@ bool deleteNode(int nim) {
         START = current->next;
     return true;
 }
+bool listEmpty() {
+    if (START == NULL)
+        return true;
+    else
+        return false;
+}
 int main()
 {
     
